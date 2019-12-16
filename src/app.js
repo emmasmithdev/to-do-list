35191c6482b32b5d5e3ad041ccbb8f0ea9fd91cb
Emma <emmagray183@gmail.com>
2019-12-16 20:59:45 +0000

    form ul app css

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
     el: "#app",
     data: {
       thingsToDo: [
-        {}
-      ]
+        {name: "hoover", highPriority: true},
+        {name: "feed cats", highPriority: true},
+        {name: "wash car", highPriority: false}
+      ],
+      newThingToDo: "",
+      newPriority: false
+    },
+    methods: {
+      saveNewThingToDo: function(){
+        let newToDoObject= {name: this.newThingToDo, highPriority: this.newPriority}
+        this.thingsToDo.push(newToDoObject)
+        this.newThingToDo = ""
+        this.newPriority = false
+      },
+      makeHighPriority: function(index){
+        this.thingsToDo[index].highPriority = true
+      }
     }
-  })
-})
+  });
+});
